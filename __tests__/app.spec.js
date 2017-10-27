1ac7a460653098e23436cf7998d7ae02de8d719b
avania <avani.amin96@gmail.com>
2017-10-27 11:17:31 -0700

    Added test

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -4,3 +4,26 @@ import {greet, invertCase} from '../js/app'; //import the functions to test!
 
 /* tests go here */
 
+describe('Basic math', function(){
+    //tests: use it or test (same thing but it goes with describe)
+    it('should add numbers together correctly', function() {
+        //javascript
+        let result = 1+1;
+        
+        //assertion
+        expect(result).toEqual(2);
+    });
+})
+
+describe('invertCase() function', function(){
+    it('correct lower case output', function(){
+        let input = "abcdefghijklmnopqrstuvwxyz";
+        let result = invertCase(input);
+        expect(result).toEqual("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
+    it ('correct upper case output', function() {
+        let input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let result = invertCase(input);
+        expect(result).toEqual("abcdefghijklmnopqrstuvwxyz");
+    });
+});
